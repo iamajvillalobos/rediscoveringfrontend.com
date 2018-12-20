d500511364ec8e53e90ffa71f3427387ed771bee
AJ Villalobos <hi@ajvillalobos.com>
2018-12-20 15:58:37 +0800

    Adjust logo

--- src/components/NavigationHeader.vue
+++ src/components/NavigationHeader.vue
@@ -11,7 +11,9 @@
           >
             <path d="M13 8V0L8.11 5.87 3 12h4v8L17 8h-4z"></path>
           </svg>
-          <h1 class="text-3xl font-medium tracking-normal">rediscoveringfrontend</h1>
+          <h1
+            class="text-3xl font-medium tracking-normal text-grey-darkest pb-1"
+          >rediscoveringfrontend</h1>
         </div>
       </router-link>
     </div>
@@ -20,16 +22,16 @@
     <div class="flex flex-row justify-center p-2">
       <ul class="flex flex-row list-reset">
         <li>
-          <router-link class="link" to="/about">About</router-link>
+          <router-link class="link" to="/about" active-class="active">About</router-link>
         </li>
         <li>
-          <router-link class="link" to="/archives">Archives</router-link>
+          <router-link class="link" to="/archives" active-class="active">Archives</router-link>
         </li>
         <li>
-          <router-link class="link" to="/now">Now</router-link>
+          <router-link class="link" to="/now" active-class="active">Now</router-link>
         </li>
         <li>
-          <router-link class="link" to="/contact">Contact</router-link>
+          <router-link class="link" to="/contact" active-class="active">Contact</router-link>
         </li>
       </ul>
     </div>
@@ -49,6 +51,10 @@ li {
 
 .link {
   @apply no-underline text-sm uppercase text-green-dark tracking-wide;
+}
+
+.active {
+  @apply pb-2 border-b border-green;
 }
 </style>
 
