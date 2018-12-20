dcc33751d7efc4030b3aea4968d6ab60f787955f
AJ Villalobos <hi@ajvillalobos.com>
2018-12-20 13:59:31 +0800

    Add tailwindcss & proxima-nova font

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <div class="font-sans">
+    <div>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
@@ -9,21 +9,10 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@tailwind preflight;
+@tailwind components;
+/**
+Add custom components here like .btn, .box etc
+**/
+@tailwind utilities;
 </style>
